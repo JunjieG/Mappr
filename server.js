df08adbb338f1d95dfcd7d240785597eca759959
Junjie Ge <jg5324@nyu.edu>
2019-11-22 15:55:57 -0500

    done setting up express and react

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+var app = express();
+
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 8080;
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
