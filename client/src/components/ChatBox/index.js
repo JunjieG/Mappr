74f1e2bf9501c66bad6fa7ccc1b44d133395cd3f
Junjie Ge <jg5324@nyu.edu>
2019-12-06 00:24:39 -0500

    did some clean up, more clean up needed

--- client/src/components/ChatBox/index.js
+++ client/src/components/ChatBox/index.js
@@ -1,72 +1,77 @@
-import React, { useRef, useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import './ChatBox.css';
+import React, { useRef, useState, useEffect } from "react";
+import io from "socket.io-client";
+
+import Contact from "./Contact";
+
+import "./ChatBox.css";
 
 let socket;
 
 export default function ChatBox({ user }) {
+  // States
   const [contacts, setContacs] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [currentChatName, setCurrentChatName] = useState('');
-  const [currentChatUID, setCurrentChatUID] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentChatName, setCurrentChatName] = useState("");
+  const [currentChatUID, setCurrentChatUID] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  
+  // References
   const chatTab = useRef();
   const fl = useRef();
+  const refs = [fl, chatTab];
 
-  const [uid, setUid] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:8080';
+  const ENDPOINT = "localhost:8080";
 
   // Connect to socket.io once the ENDPOINT is created (when website opens)
   useEffect(() => {
     socket = io(ENDPOINT);
-    
+
     return () => {
-      socket.emit('disconnect');
+      socket.emit("disconnect");
       socket.off();
-    }
-  }, [ENDPOINT])
+    };
+  }, [ENDPOINT]);
 
   useEffect(() => {
     if (user) {
-      socket.emit('join', { uid: user.uid, room: 'New York' }, () => {
-      });
+      socket.emit("join", { uid: user.uid, room: "New York" }, () => {});
     }
-  }, [user])
+  }, [user]);
 
   useEffect(() => {
-    socket.on('message', (message) => {
+    socket.on("message", message => {
       setMessages([...messages, message]);
-    })
+    });
     console.log(messages);
-  }, [messages])
+  }, [messages]);
 
   // sending messages
-  const sendMessage = (e) => {
+  const sendMessage = e => {
     e.preventDefault();
     if (message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+      socket.emit("sendMessage", message, () => setMessage(""));
     }
-  }
+  };
 
-  const privateMessage = (e) => {
+  const privateMessage = e => {
     e.preventDefault();
     if (message) {
-      socket.emit('privateMessage', currentChatUID,message, ({ error }) => {
+      socket.emit("privateMessage", currentChatUID, message, ({ error }) => {
         if (error) {
           console.log("Private message could not be sent: ", error);
         }
-        setMessage('')
+        setMessage("");
       });
-      setMessages([...messages, {user: user.uid, text: message}]);
+      setMessages([...messages, { user: user.uid, text: message }]);
     }
-  }
+  };
 
-  const searchUser = (e) => {
+  const searchUser = e => {
     e.preventDefault();
     if (searchQuery) {
-      socket.emit('findContact', searchQuery, ({ error, user }) => {
-        setSearchQuery('')
+      socket.emit("findContact", searchQuery, ({ error, user }) => {
+        setSearchQuery("");
         if (user) {
           setContacs([...contacts, user]);
           console.log(contacts);
@@ -76,125 +81,59 @@ export default function ChatBox({ user }) {
         }
       });
     }
-  }
-
-  const User = ({ contact }) => {
-    // Get the uid of the contact for messaging
-    let contactUID = (contact && contact.uid);
-
-    const contactClick = (e) => {
-      // Create user profile picture when clicked
-      let clone = e.currentTarget.childNodes[0].cloneNode(true);
-      let childTop = e.currentTarget.offsetTop - e.currentTarget.parentNode.parentNode.offsetTop;
-      let top = childTop + 12 + 'px';
-      clone.style.top = top;
-      clone.classList.add('floatingImg');
-      document.querySelector('#profile').appendChild(clone);
-
-      function move(elem) {
-          elem.style.left = '108px';
-          elem.style.top = '30px';
-          let width = 0;
-          function frame() {
-              width++  // update parameters
-              elem.style.width = width + 'px' // show frame
-              if (width == 68)  // check finish condition
-                  clearInterval(id)
-          }
-          var id = setInterval(frame, 10) // draw every 10ms
-      }
-      
-      move(document.querySelector('.floatingImg'));
-
-      // Reset chat name
-      setCurrentChatName('');
-
-      // Fade out friendlist and fade in chat box
-      fl.current.classList.toggle('fade');
-      chatTab.current.style.display = 'block';
-      setTimeout(function(){ chatTab.current.classList.toggle('fade') }, 10);
-      setCurrentChatName(e.currentTarget.childNodes[1].childNodes[0].innerHTML);
-      setCurrentChatUID(contactUID);
-      document.querySelector('#profile p').classList.add('animate');
-      setTimeout(function() {
-        // Display chat messages
-        document.querySelector('#chat-messages').classList.add('animate');
-
-        // Close button
-        document.querySelector('.cx').classList.add('s1');
-        document.querySelector('.cy').classList.add('s1');
-        setTimeout(function(){
-          document.querySelector('.cx').classList.add('s2');
-          document.querySelector('.cy').classList.add('s2');
-        }, 100);
-        setTimeout(function(){
-          document.querySelector('.cx').classList.add('s3');
-          document.querySelector('.cy').classList.add('s3');
-        }, 200);
-      }, 100)
-    }
-
-
-    return (
-      <div className="friend" onClick={((e) => contactClick(e))}>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/5_copy.jpg" />
-          <p>
-            <strong>{ contact.uid }</strong>
-            <span>Last Message</span>
-          </p>
-      </div>
-    )
-  }
+  };
 
   const Message = ({ message }) => {
     if (message.user.trim().toLowerCase() === currentChatUID) {
-      console.log('user');
+      console.log("user");
       return (
         <div className="message">
-            <div className="bubble">
-              { message.text }
-                <span>3 min</span>
-            </div>
+          <div className="bubble">
+            {message.text}
+            <span>3 min</span>
+          </div>
         </div>
-      )
+      );
     } else if (message.user === user.uid) {
-      console.log('self');
+      console.log("self");
       return (
         <div className="message right">
-            <div className="bubble">
-              { message.text }
-                <span>3 min</span>
-            </div>
+          <div className="bubble">
+            {message.text}
+            <span>3 min</span>
+          </div>
         </div>
-      )
+      );
     } else {
-      console.log('other people', message.user);
-      return (null);
+      console.log("other people", message.user);
+      return null;
     }
-  }
+  };
 
   const closeClick = () => {
-    let profileImg = document.querySelector('.floatingImg');
-    
-    document.querySelector('#chat-messages').classList.remove('animate');
-    document.querySelector('#profile').classList.remove('animate');
-    document.querySelector('#profile p').classList.remove('animate');
+    let profileImg = document.querySelector(".floatingImg");
+
+    document.querySelector("#chat-messages").classList.remove("animate");
+    document.querySelector("#profile").classList.remove("animate");
+    document.querySelector("#profile p").classList.remove("animate");
     // Fade in friendlist and fade out chat box
-    chatTab.current.classList.toggle('fade');
-    setTimeout(function(){ fl.current.classList.toggle('fade') }, 10);
-    document.querySelector('#profile p').classList.remove('animate');
+    chatTab.current.classList.toggle("fade");
+    setTimeout(function() {
+      fl.current.classList.toggle("fade");
+    }, 10);
+    document.querySelector("#profile p").classList.remove("animate");
 
     // Close button
-    document.querySelector('.cx').classList.remove('s1', 's2', 's3');
-    document.querySelector('.cy').classList.remove('s1', 's2', 's3');
-    setTimeout(function(){
-      chatTab.current.style.display = 'none'
+    document.querySelector(".cx").classList.remove("s1", "s2", "s3");
+    document.querySelector(".cy").classList.remove("s1", "s2", "s3");
+    setTimeout(function() {
+      chatTab.current.style.display = "none";
       profileImg.parentNode.removeChild(profileImg);
-      setCurrentChatUID('');
+      setCurrentChatUID("");
     }, 700);
-  }
+  };
 
-  return ( 
+  return (
     <div id="chatbox">
       <div id="friendslist" ref={fl}>
         <div id="topmenu">
@@ -202,57 +141,59 @@ export default function ChatBox({ user }) {
           <span className="room"></span>
           <span className="history"></span>
         </div>
-            
-        <div id="friends">
 
-          { contacts.map(( contact, i ) => (
-            <User key={i} contact={contact} />
-          )) }
-                
+        <div id="friends">
+          {contacts.map((contact, i) => (
+            <Contact
+              key={i}
+              contact={contact}
+              setCurrentChatName={setCurrentChatName}
+              setCurrentChatUID={setCurrentChatUID}
+              references={refs}
+            />
+          ))}
+
           <div id="search">
-            <input 
+            <input
               type="text"
               id="searchfield"
               placeholder="Search contacts..."
               value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              onKeyPress={(e) => e.key === 'Enter' ? searchUser(e) : null}
+              onChange={e => setSearchQuery(e.target.value)}
+              onKeyPress={e => (e.key === "Enter" ? searchUser(e) : null)}
             />
           </div>
-        </div>                
-      </div>	
-        
-      <div id="chatview" className="p1 fade" ref={chatTab}>    	
-          <div id="profile">
+        </div>
+      </div>
 
-              <div id="close" onClick={() => closeClick()}>
-                  <div className="cy"></div>
-                  <div className="cx"></div>
-              </div>
-              
-              <p>{currentChatName}</p>
+      <div id="chatview" className="p1 fade" ref={chatTab}>
+        <div id="profile">
+          <div id="close" onClick={() => closeClick()}>
+            <div className="cy"></div>
+            <div className="cx"></div>
           </div>
 
-          <div id="chat-messages">
-            <label>Thursday 02</label>
-            { messages.map(( message, i ) => (
-              <Message key={i} message={message} />
-            )) }
-          </div>
-        
-          <div id="sendmessage">
-            <input
-              type='text'
-              value={message}
-              placeholder='Message...'
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyPress={(e) => e.key === 'Enter' ? privateMessage(e) : null}
-            />
-            <button id="send"></button>
-          </div>
-      
-      </div>        
-    </div>	
-    
-  )
+          <p>{currentChatName}</p>
+        </div>
+
+        <div id="chat-messages">
+          <label>Thursday 02</label>
+          {messages.map((message, i) => (
+            <Message key={i} message={message} />
+          ))}
+        </div>
+
+        <div id="sendmessage">
+          <input
+            type="text"
+            value={message}
+            placeholder="Message..."
+            onChange={e => setMessage(e.target.value)}
+            onKeyPress={e => (e.key === "Enter" ? privateMessage(e) : null)}
+          />
+          <button id="send"></button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
-const { addUser, removeUser, getUser, getUserUID, getUserInRoom } = require('./users.js')
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUserUID,
+  getUserInRoom
+} = require("./users.js");
 
 var app = express();
 
@@ -12,7 +18,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = socketio(server);
 
-const router = require('./router.js');
+const router = require("./router.js");
 
 //// Firebase App (the core Firebase SDK) is always required and
 //// must be listed before other Firebase SDKs
@@ -22,60 +28,71 @@ const router = require('./router.js');
 //require("firebase/firestore");
 
 //const firebaseConfig = {
-  //apiKey: "AIzaSyAucD4dS2Dj3iCfyMYaeQwppCpZaQYGnLo",
-  //authDomain: "mappr-1574369019968.firebaseapp.com",
-  //databaseURL: "https://mappr-1574369019968.firebaseio.com",
-  //projectId: "mappr-1574369019968",
-  //storageBucket: "mappr-1574369019968.appspot.com",
-  //messagingSenderId: "604941514326",
-  //appId: "1:604941514326:web:e15732396ac1961efd6df7"
+//apiKey: "AIzaSyAucD4dS2Dj3iCfyMYaeQwppCpZaQYGnLo",
+//authDomain: "mappr-1574369019968.firebaseapp.com",
+//databaseURL: "https://mappr-1574369019968.firebaseio.com",
+//projectId: "mappr-1574369019968",
+//storageBucket: "mappr-1574369019968.appspot.com",
+//messagingSenderId: "604941514326",
+//appId: "1:604941514326:web:e15732396ac1961efd6df7"
 //};
 
 //// Initialize Firebase
 //firebase.initializeApp(firebaseConfig);
 
-io.on('connection', (socket) => {
-  console.log('someone has connected');
+io.on("connection", socket => {
+  console.log("someone has connected");
 
-  socket.on('join', ({ uid, room }, callback) => {
+  socket.on("join", ({ uid, room }, callback) => {
     const { user } = addUser({ socket_id: socket.id, uid, room });
     console.log(uid);
-    socket.emit('message', { user: 'server', text: `Welcome ${user.uid} to ${user.room} chatroom`})
-    socket.broadcast.to(user.room).emit('message', { user: 'server', text: `${user.uid} has successfully connected`})
+    socket.emit("message", {
+      user: "server",
+      text: `Welcome ${user.uid} to ${user.room} chatroom`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", {
+        user: "server",
+        text: `${user.uid} has successfully connected`
+      });
     socket.join(user.room);
     callback();
-  })
+  });
 
-  socket.on('sendMessage', (message, callback) => {
+  socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit('message', { user: user.uid, text: message });
-  })
+    io.to(user.room).emit("message", { user: user.uid, text: message });
+  });
 
-  socket.on('privateMessage', (receiverUID, message, callback) => {
+  socket.on("privateMessage", (receiverUID, message, callback) => {
     const sender = getUser(socket.id);
     const receiver = getUserUID(receiverUID);
     if (receiver) {
-      io.to(`${receiver.socket_id}`).emit('message', { user: sender.uid, text: message });
+      io.to(`${receiver.socket_id}`).emit("message", {
+        user: sender.uid,
+        text: message
+      });
     } else {
-      callback({ error: 'Target person not found' });
+      callback({ error: "Target person not found" });
     }
-  })
+  });
 
-  socket.on('findContact', (uid, callback) => {
+  socket.on("findContact", (uid, callback) => {
     uid = uid.trim().toLowerCase();
     const user = getUserUID(uid);
     if (user) {
       callback({ user });
     } else {
-      callback({ error: 'User not found' });
+      callback({ error: "User not found" });
     }
     console.log(user);
-  })
+  });
 
-  socket.on('disconnect', () => {
-    console.log('use has left');
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("use has left");
+  });
+});
 
 app.use(router);
 
